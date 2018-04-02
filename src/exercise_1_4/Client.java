21c00791ce8a07228d4a296599f5d6ee7384ee4c
Unknown <p.starzacher@gmail.com>
2018-04-02 16:11:03 +0200

    Exercise 1.4  Server-Client communication first draft

--- src/exercise_1_4/Client.java
+++ src/exercise_1_4/Client.java
@@ -1,5 +1,33 @@
 package exercise_1_4;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("192.168.1.113", 1234);
+			Scanner scn = new Scanner(System.in);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+			while(socket.isConnected()) {
+				out.write(scn.nextLine());
+				out.flush();
+				System.out.println(in.readLine());
+			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/exercise_1_4/Server.java
+++ src/exercise_1_4/Server.java
@@ -1,5 +1,26 @@
 package exercise_1_4;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
 
+	public static void main(String [] args) {
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(1234);
+			List<ClientThread> clients = new ArrayList<>();
+			while(true) {	
+				ClientThread newClient = ClientThread.getInstance(serverSocket.accept());
+				newClient.start();
+				newClient.writeToStream("hello ");
+				clients.add(newClient);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		
+	}
+	}
 }
