4f03ed88cafb04cde05f1f61135e003a9bb3f51f
Unknown <p.starzacher@gmail.com>
2018-04-09 21:29:36 +0200

    Merge and implementing download for client
    somehow the download doenst finish on the client side....there always some kBytes missing

--- assignment_2/exercise_2_2/Client.java
+++ assignment_2/exercise_2_2/Client.java
@@ -1,11 +1,17 @@
 package exercise_2_2;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Client {
@@ -13,36 +19,51 @@ public class Client {
 	private Scanner scn;
 	private BufferedReader in;
 	private PrintWriter out;
-	
-	public void startClient() {	
+	private Socket socket;
+
+	public void startClient() {
 		scn = new Scanner(System.in);
-		
+
 		System.out.println("Enter port to connect: ");
 		Integer port = Integer.parseInt(scn.nextLine());
-		
+
 		try {
-			Socket socket = new Socket("192.168.1.109", port);
+			 socket = new Socket("192.168.1.109", port);
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			
-			while(socket.isConnected()) {
+
+			while (socket.isConnected()) {
 				String input = in.readLine();
+				System.out.println("server: " + input);
+				
+				if (input.startsWith("file incoming")) {
+					downloadFile();
+				}
+				
 				writeLine();
+
 			}
-			
-			
-		} catch (IOException e) {
+
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
-		}	
+		}
 	}
 
-	private  void writeLine() {
+	private void writeLine() {
 		String in = scn.nextLine();
 		out.println(in);
+		out.flush();
 	}
-	
+
 	public static Client getClient() {
 		return new Client();
+	}
+	
+	public void downloadFile() throws IOException, InterruptedException {
+		File nf = new File("recieved" + new Date().getTime());
+		System.out.println( Files.copy(socket.getInputStream(), nf.toPath(),StandardCopyOption.REPLACE_EXISTING));
+		System.out.println("Finished downloading file");
+		System.out.println(nf.renameTo(new File(in.readLine())));
 	}
 
 }
